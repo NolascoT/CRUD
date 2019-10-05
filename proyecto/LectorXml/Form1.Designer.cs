de831098ee051b6c18cd8a9d3f4dadd2d60894a4
cortescal <51030447+cortescal@users.noreply.github.com>
2019-10-04 20:58:30 -0500

    Se realiza proceso de xml de proyecto, eliminando datos que no sirven, quedando nombre de clases y atributos

---
+++ proyecto/LectorXml/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LectorXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Examinar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Procesar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Crear Herencia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 147);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 265);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(224, 147);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(306, 265);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ proyecto/LectorXml/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+namespace LectorXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog buscar = new OpenFileDialog();
+            string extension;
+
+            if(buscar.ShowDialog() == DialogResult.OK)
+            {
+                extension = Path.GetExtension(buscar.FileName);
+                if(extension==".xml")
+                {
+                    textBox1.Text = buscar.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("Archivo Incorrecto");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string umldiagrams = "umldiagrams";
+            string UMLClassDiagram = "UMLClassDiagram";
+            string UMLClass = "UMLClass";
+            string superitem = "superitem";
+            string UMLAssociation = "UMLAssociation";
+            string point = "point";
+            string UMLGeneralization = "UMLGeneralization";
+            string valueAst = "value=\"*\"";
+            string valueUno = "value=\"1\"";
+            string valueNulo = "value=\"\"";
+            char quitalo = '+';
+            char quitalo2 = ':';
+
+            string line;
+
+            string NewXml = "";
+            StreamReader seCargoXml = new StreamReader(textBox1.Text);
+            
+            while ((line = seCargoXml.ReadLine()) != null)
+            {
+                if (line.Contains(UMLClassDiagram) || line.Contains(UMLClass)
+                    || line.Contains(superitem) || line.Contains(UMLAssociation)
+                    || line.Contains(point) || line.Contains(UMLGeneralization)
+                    || line.Contains(valueAst) || line.Contains(valueUno)
+                    || line.Contains(valueNulo))
+                {
+                }
+                else
+                {
+                    if (line.Contains(quitalo))
+                    {
+                        line = line.Replace("+", "");
+                    }
+                    if (line.Contains(quitalo2))
+                    {
+                        line = line.Replace(":", " ");
+                    }
+                    NewXml += line;
+                    richTextBox1.Text = NewXml;
+                }
+            } 
+            seCargoXml.Close();
+            Console.WriteLine(NewXml);
+
+            //XmlTextReader reader = new XmlTextReader(NewXml);
+
+            /*while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        //if (line.Contains(UMLClassDiagram)
+                          //  {}
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                            break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+            Console.ReadLine();*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
