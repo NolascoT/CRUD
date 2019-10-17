e6e024ca2f5813f5005eb7f14e6d92cbacd32211
cortescal <51030447+cortescal@users.noreply.github.com>
2019-10-16 19:40:19 -0500

    Se establece herencia, mencionando clase heredada, se guarda resultado en un archivo xml

--- proyecto/LectorXml/Form1.Designer.cs
+++ proyecto/LectorXml/Form1.Designer.cs
@@ -34,11 +34,23 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(618, 90);
+            this.button1.Location = new System.Drawing.Point(435, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +60,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 182);
+            this.button2.Location = new System.Drawing.Point(435, 29);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -58,50 +70,135 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(618, 284);
+            this.button3.Location = new System.Drawing.Point(418, 87);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(96, 57);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Crear Herencia";
+            this.button3.Text = "Establecer Herencia";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 90);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(393, 22);
             this.textBox1.TabIndex = 3;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 147);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(193, 265);
+            this.richTextBox1.Size = new System.Drawing.Size(258, 285);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(224, 147);
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(306, 265);
+            this.richTextBox2.Size = new System.Drawing.Size(514, 285);
             this.richTextBox2.TabIndex = 5;
             this.richTextBox2.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(12, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 285);
+            this.panel1.TabIndex = 6;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(776, 285);
+            this.splitContainer1.SplitterDistance = 258;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(418, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Obtener clases";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(174, 63);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Crear Herencia";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(149, 104);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Guardar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +212,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- proyecto/LectorXml/Form1.cs
+++ proyecto/LectorXml/Form1.cs
@@ -11,7 +11,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Xml;
 using System.Xml.Linq;
-
+using System.Text.RegularExpressions;
 
 namespace LectorXml
 {
@@ -183,65 +183,97 @@ namespace LectorXml
             string text = writer.ToString();
             Console.Write(text);
             richTextBox2.Text = writer.ToString();
-            /*
-             * resultado=<umldiagrams>
-	<UMLClass>
-		<Id>Platillo</Id>
-		<atributo>nombre  string </atributo>
-		<atributo>precio float </atributo>
-		<atributo>descripcion string</atributo>
-	</UMLClass>
-	<UMLClass>
-		<Id>Empleado</Id>
-		<atributo>nombre string</atributo>
-		<atributo>apellidosp string </atributo>
-		<atributo>apellidom string</atributo>
-		<atributo>matricula int</atributo>
-	</UMLClass>
-	<UMLClass>
-		<Id>Chef</Id>
-		<atributo>sueldo float</atributo>
-		<atributo>cargo  string</atributo>
-		<atributo>especialidad string</atributo>
-	</UMLClass>
-	<UMLClass>
-		<Id>Restaurante</Id>
-		<atributo>nombre string</atributo>
-		<atributo>domicilio  string</atributo>
-	</UMLClass>
-	<UMLClass>
-		<Id>Mesero </Id>
-		<atributo>mesa int</atributo>
-		<atributo>sueldo float</atributo>
-	</UMLClass>
-	<UMLClass>
-		<Id>Encargado </Id>
-		<atributo>sucursal string</atributo>
-	</UMLClass>
-	<Id>tiene</Id>
-	<Id>tiene </Id>
-</umldiagrams>*/
-
-            var doc = XDocument.Parse(text);
-
-            var result = from umldiagrams in doc.Descendants("umldiagrams")
-                         select new
-                         {                             
-                             UMLClass = new
-                             {
-                                 Id = umldiagrams.Element("UMLClass").Element("Id").Value
-                             }
-                         };
-
-            for (int j=0;j<=result.Count();j++)
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            int x = this.richTextBox2.Lines.Count();
+            for (int i = 0; i <= this.richTextBox2.Lines.Count() - 1; i++)
             {
-                Console.WriteLine(j);
+                if (richTextBox2.Lines[i].Contains("<Id>"))
+                {
+                    richTextBox1.Text += richTextBox2.Lines[i].Replace("<Id>", "").Replace("</Id>", "").Trim() + "\n";
+                    comboBox1.Items.Add(richTextBox2.Lines[i].Replace("<Id>", "").Replace("</Id>", "").Trim());
+                }
+            }        
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox2.ResetText();
+            comboBox2.Items.Clear();
+            if (comboBox1.SelectedIndex<=-1)
+            {
+                MessageBox.Show("No has seleccionado algun valor");
+            }
+            else
+            {
+                string seleccion = "";
+                seleccion = comboBox1.SelectedItem.ToString();
+                int x = this.richTextBox1.Lines.Count();
+                for (int i = 0; i <= this.richTextBox1.Lines.Count()-1; i++)
+                {
+                    if (richTextBox1.Lines[i].Contains(seleccion))
+                    {
+                    }
+                    else
+                    {                      
+                        comboBox2.Items.Add(richTextBox1.Lines[i]);
+                    }
+                }
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex <= -1 && comboBox2.SelectedIndex <= -1)
+            {
+                MessageBox.Show("No has seleccionado algun valor");
+            }
+            else
+            {
+                string hijo = "";
+                string padre = "";
+                hijo = comboBox1.SelectedItem.ToString();
+                padre = comboBox2.SelectedItem.ToString();
+                MessageBox.Show(hijo + " hereda de " + padre);
 
+                string busca = "<Id>" + hijo + "</Id>";
+                string agregaSiE = "<HeredaDe>" + padre + "</HeredaDe>";
+                string agrega = "<" + hijo + "><HeredaDe>" + padre + "</HeredaDe></" + hijo + ">";
+                string xml = richTextBox2.Text;
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xml);
+                XmlDocumentFragment xmlDocFragment = xmlDoc.CreateDocumentFragment();
+                xmlDocFragment.InnerXml = agrega;
+                xmlDoc.SelectSingleNode("umldiagrams").AppendChild(xmlDocFragment);
+
+                StringWriter sw = new StringWriter();
+                XmlTextWriter tx = new XmlTextWriter(sw);
+                xmlDoc.WriteTo(tx);
+
+                Console.WriteLine(sw.ToString());
+                richTextBox2.Text = "";
+                richTextBox2.Text = sw.ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";      
+            saveFileDialog1.Title = "Save text Files";
+            saveFileDialog1.CheckFileExists = false;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "xml";
+            saveFileDialog1.Filter = "Text files (*.xml)|*.xml|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string name = saveFileDialog1.FileName;
+                File.WriteAllText(name, richTextBox2.Text);
+            }
         }
     }
 }
